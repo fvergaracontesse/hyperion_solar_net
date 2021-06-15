978c4c3441793cd74190a009cf88ba48f457ff71
Felipe <felvergara@gmail.com>
2021-06-15 18:52:01 -0400

    fixed boundaries.

--- webapp/flask/app/js/main.js
+++ webapp/flask/app/js/main.js
@@ -183,7 +183,7 @@ function getObjects(type) {
                     strokeOpacity: 0.8,
                     strokeWeight: 2,
                     fillColor: "#FF0000",
-                    fillOpacity: 0.35,
+                    fillOpacity: 0,
                     map: currentMap.map,
                     bounds: bounds
               });
