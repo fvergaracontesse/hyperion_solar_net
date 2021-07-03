1ecc7d4007d8e04a872ec3f3c8f82967f583eccf
Felipe <felvergara@gmail.com>
2021-07-03 18:21:47 -0400

    add logs.

--- webapp/flask/app/offline_pipeline.py
+++ webapp/flask/app/offline_pipeline.py
@@ -76,26 +76,25 @@ if not os.path.isfile("data/"+coordinate_file + "_classification"):
     save_pickle_file("data/"+coordinate_file + "_classification", classification_tiles)
 
 
-classification_tiles = load_pickle_file("data/"+coordinate_file + "_classification")
-solar_panel_tiles = list(filter(lambda x: x["prediction"] == 1, classification_tiles))
-solar_panel_tiles_chunks = chunks(solar_panel_tiles, 50)
-
 if not os.path.isfile("data/"+coordinate_file + "_segmentation"):
+    print("START Segmentation:", datetime.now())
+    classification_tiles = load_pickle_file("data/"+coordinate_file + "_classification")
+    solar_panel_tiles = list(filter(lambda x: x["prediction"] == 1, classification_tiles))
+    solar_panel_tiles_chunks = chunks(solar_panel_tiles, 50)
     segmentation_tiles = {}
     model = Segmentation()
     i = 0
     for chunk in solar_panel_tiles_chunks:
+        print("ITERATION: ", i)
+        print("ITERATION TIME: ", datetime.now())
         result_tiles = model.predict(chunk, 21, True, place)
         segmentation_tiles.update(result_tiles)
-        if i == 3:
-            break
         i += 1
     for i, tile in enumerate(classification_tiles):
         if tile["id"] in segmentation_tiles:
             print(segmentation_tiles[tile["id"]])
             classification_tiles[i] = segmentation_tiles[tile["id"]]
     save_pickle_file("data/"+coordinate_file + "_segmentation", classification_tiles)
-
 
 
 # map_object.get_sat_maps(tiles_poly, loop, image_directory, uuid.uuid4().hex)
